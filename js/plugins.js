27105288223c9ddddc21f6b57544c9cd24d19ef4
Gagik <gagik@bigbek.com>
2016-09-01 19:02:16 +0400

    Adds escape key event handler

--- js/plugins.js
+++ js/plugins.js
@@ -178,20 +178,9 @@ function drawLetters(a) {
 
 //for form validation use https://github.com/jzaefferer/jquery-validation
 
-
-/*
- * DOC: adds jQuery function checks if element exists
- * USAGE: $('.element').exists(function(){});
- * */
-
-$.fn.exists = function(callback) {
-    var args = [].slice.call(arguments, 1);
-
-    if (this.length) {
-        callback.call(this, args);
+//escape key event for modals
+$(document).keyup(function(e) {
+    if (e.keyCode == 27) { // escape key maps to keycode `27`
+        // <DO YOUR WORK HERE>
     }
-
-    return this;
-};
-
-
+});
